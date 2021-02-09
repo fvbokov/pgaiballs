b3e5549425b32f0e354d9a51a0386579b9f16bde
Fedor <58223445+Marginalisimus@users.noreply.github.com>
2021-02-09 19:20:05 +0300

    add character

--- aiballs/character.py
+++ aiballs/character.py
@@ -7,23 +7,21 @@ from pygame.math import Vector2 as vector
 
 class Character(Circle):
     def control(self, mouse_state):
-        mouse_pos = pygame.mouse.get_pos()
+        mouse_pos = vector(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
         if pygame.mouse.get_pressed()[0] == False or mouse_state != 'pressing':
             return 0
-        if ((mouse_pos[0] - self.pos.x)**2 + (mouse_pos[1] - self.pos.y)**2) ** 0.5 > self.radius + 75: return 0
-        direction = vector(mouse_pos[0] - self.pos.x, mouse_pos[1] - self.pos.y)
-        self.velocity += -direction.normalize() * 15
+        #if ((mouse_pos.x - self.pos.x)**2 + (mouse_pos.y - self.pos.y)**2) ** 0.5 > self.radius + 75: return 0
+        direction = vector(mouse_pos.x - self.pos.x, mouse_pos.y - self.pos.y)
+        self.velocity -= direction.normalize() * 15
 
         split_mass = self.mass / 30
-        new_mass = self.mass - split_mass
-
-        self.mass = new_mass
+        self.mass -= split_mass
 
         child = Circle(self.pos.x, self.pos.y, 0, pygame.Color("Green"))
         child.mass = split_mass
-        child.pos = vector(mouse_pos[0], mouse_pos[1])
+        child.pos = mouse_pos - self.pos
+        child.pos.scale_to_length(self.radius + child.radius)
+        child.pos += self.pos
         child.velocity = direction.normalize() * 80
-
-        print(child.pos)
 
         return child
