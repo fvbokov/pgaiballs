a594991af24c7d3e9fb00b230c023a063283b9f6
Fedor <58223445+Marginalisimus@users.noreply.github.com>
2021-04-29 19:12:30 +0300

    add notepad++ autodownload

--- aiballs/game.py
+++ aiballs/game.py
@@ -1,9 +1,31 @@
 """A class in which game starts and ends."""
 import pygame
+from platform import architecture
+from urllib.request import urlretrieve
+from pathlib import Path
+from shutil import rmtree
+from zipfile import ZipFile
 
 class Game:
     @classmethod
-    def init(cls, width = 800, height = 600):
+    def init(cls, width = 1600, height = 900):
+        path_to_notepad = Path(__file__).parent/'data/notepad++/notepad++.exe'
+        if not path_to_notepad.exists():
+            path_to_tempdir = Path(__file__).parent/'data/temp'
+            path_to_tempdir.mkdir()
+
+            if architecture()[0] == '64bit':
+                urlretrieve(url='https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.5/npp.7.9.5.portable.x64.zip', 
+                    filename=path_to_tempdir/'notepad++.zip')
+            else:
+                urlretrieve(url='https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.5/npp.7.9.5.portable.zip', 
+                    filename=path_to_tempdir/'notepad++.zip')
+            path_to_notepad.parent.mkdir()
+            with ZipFile(path_to_tempdir/'notepad++.zip', 'r') as zip:
+                zip.extractall(path_to_notepad.parent)
+
+            rmtree(path_to_tempdir)
+
         pygame.init()
 
         cls.window = pygame.display.set_mode((width, height))
