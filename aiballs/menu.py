0435b1d2741496ec730a5dba5775befb569a6960
Fedor <58223445+Marginalisimus@users.noreply.github.com>
2021-04-29 19:12:46 +0300

    add buttons to main menu

--- aiballs/menu.py
+++ aiballs/menu.py
@@ -11,12 +11,16 @@ from .fps import FpsDisplay
 
 class Menu(Scene):
     def play(self):
-        manager = pygame_gui.UIManager((Game.window.get_width(), Game.window.get_height()))
-        button_play = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (200, 50)),
-                            text='play', manager=manager)
+        manager = pygame_gui.UIManager((Game.window.get_width(), Game.window.get_height()), 
+            os.path.dirname(__file__) + '/data/button_theme/theme.json')
+        button_play = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 275), (300, 100)),
+                            text='Play', manager=manager)
+        button_options = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((650, 450), (300, 100)),
+                            text='Options', manager=manager)
+        button_quit = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((1000, 650), (300, 100)),
+                            text='Quit', manager=manager)
         
-        
-        background = pygame.image.load(os.path.dirname(__file__) + f'/data/images/menu_background.png')
+        background = pygame.image.load(os.path.dirname(__file__) + '/data/images/menu_background2.png')
         background = pygame.transform.scale(background, (Game.window.get_width(), Game.window.get_height()))
 
         clock = pygame.time.Clock()
@@ -36,6 +40,7 @@ class Menu(Scene):
 
             manager.update(dt)
 
+            
             Game.window.blit(background, (0, 0)) #---------
 
             manager.draw_ui(Game.window)
